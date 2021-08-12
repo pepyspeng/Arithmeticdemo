79a8d37e39c34af5af57317792fe6589d7b50023
zhu.pengpeng <zhu.pengpeng@immomo.com>
2021-08-12 20:31:38 +0800

    suanfa

--- lib/src/main/java/j/com/lib/offer/offer_38_MoreThanHalfNum_Solution.java
+++ lib/src/main/java/j/com/lib/offer/offer_38_MoreThanHalfNum_Solution.java
@@ -20,4 +20,34 @@ public class offer_38_MoreThanHalfNum_Solution {
         long count =Arrays.stream(array).filter(i -> (i == mid)).count();
         return count>array.length/2 ? mid:-1;
     }
+
+    public static int MoreThanHalfNum_Solution_1(int[] array) {
+        //候选人
+        int cond = -1;
+        //票数
+        int cnt = 0;
+        for (int a:array){
+            if(cnt == 0){
+                cond = a;
+                cnt ++;
+            }else{
+                if(a == cond){
+                    cnt ++;
+                }else{
+                    cnt--;
+                }
+            }
+        }
+        cnt = 0;
+        for (int a:array){
+            if(a == cond){
+                cnt ++;
+            }
+        }
+        if(cnt > array.length/2){
+            return cond;
+        }else{
+            return 0;
+        }
+    }
 }
