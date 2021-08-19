03674a459cfc5c80d8bdf407f2cee4fc681af9b9
zhu.pengpeng <zhu.pengpeng@immomo.com>
2021-08-19 14:08:37 +0800

    topk

---
+++ lib/src/main/java/j/com/lib/offer/offer_40_sumOfSubArray.java
@@ -0,0 +1,27 @@
+package j.com.lib.offer;
+
+
+/**
+ * @author     : zhupp
+ * @date       : 2021/8/19
+ * description : 数组中连续最大和
+ */
+public class offer_40_sumOfSubArray {
+    public static void main(String[] args) {
+        int[] array = new int[]{1, -2, 3, 10, -4, 7, 2, -5};
+        System.out.println(FindGreatestSumOfSubArray(array));
+    }
+
+
+
+
+    public static int FindGreatestSumOfSubArray(int[] array) {
+        int result = array[0];
+        int temp = 0;
+        for (int a : array) {
+            temp = Math.max(temp+a,a);
+            result = Math.max(result, temp);
+        }
+        return result;
+    }
+}
--- lib/src/main/java/j/com/lib/offer/offer_4_replaceSpace.java
+++ lib/src/main/java/j/com/lib/offer/offer_4_replaceSpace.java
@@ -24,4 +24,28 @@ public class offer_4_replaceSpace {
         return  sb.toString();
     }
 
+
+    /**
+     * 去掉空格+首字符大写
+     * @param s
+     */
+    public static void toUpperCase(String s){
+        StringBuilder sb = new StringBuilder(s);
+        if(sb.charAt(0) >='a' && sb.charAt(0) <='z'){
+            sb.setCharAt(0, (char) (sb.charAt(0)-32));
+        }
+        for (int i =1;i<sb.length();i++){
+            if(sb.charAt(i-1) == ' ' &&(sb.charAt(i) >='a' && sb.charAt(i) <='z')){
+                sb.setCharAt(i, (char) (sb.charAt(i)-32));
+            }
+        }
+        for (int i =1;i<sb.length();i++){
+            if(sb.charAt(i) == ' '){
+                sb.deleteCharAt(i);
+                i--;
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
 }
