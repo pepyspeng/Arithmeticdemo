51112b232aab365dc04bc9d1a8cd67976d139ee2
zhu.pengpeng <zhu.pengpeng@immomo.com>
2021-09-02 10:03:13 +0800

    是否平衡二叉树

---
+++ lib/src/main/java/j/com/lib/offer/offer_48_IsBalanced_Solution.java
@@ -0,0 +1,34 @@
+package j.com.lib.offer;
+
+import j.com.lib.bean.TreeNode;
+
+/**
+ * @author : zhupp
+ * @date : 2021/9/2
+ * description : 判断一棵树是否是平衡二叉树
+ */
+public class offer_48_IsBalanced_Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean IsBalanced_Solution(TreeNode root) {
+        DepthTree(root);
+        return isBalanced;
+    }
+
+    private boolean isBalanced = true;
+
+    public int DepthTree(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = DepthTree(node.left);
+        int right = DepthTree(node.right);
+        if (Math.abs(right - left) > 1) {
+            isBalanced = false;
+        }
+        return Math.max(left, right) + 1;
+    }
+}
