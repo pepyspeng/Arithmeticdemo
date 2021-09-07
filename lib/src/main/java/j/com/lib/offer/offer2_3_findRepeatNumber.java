3de08ffff30f014e19e66366da556a85c446b063
zhu.pengpeng <zhu.pengpeng@immomo.com>
2021-09-07 20:16:46 +0800

    数组中两个和为s的

--- lib/src/main/java/j/com/lib/offer/offer2_3_findRepeatNumber.java
+++ lib/src/main/java/j/com/lib/offer/offer2_3_findRepeatNumber.java
@@ -9,28 +9,79 @@ import java.util.HashMap;
 public class offer2_3_findRepeatNumber {
 
     public static void main(String[] args) {
-        int[] array =new int[]{1, 1, 1};
-        System.out.println(findRepeatNumber(array));
+        int[] array = new int[]{1, 8, 1, 9, 2, 6, 5, 1, 4, 5};
+        System.out.println(findRepeatNumber1(array));
     }
 
-    static HashMap<Integer,Integer> map = new HashMap<>();
+    public static int search(int[] array, int target) {
+        int left = binarySearch(array, target, true);
+        int right = binarySearch(array, target, false) - 1;
+        if (left <= right && right < array.length
+                && array[left] == target && array[right] == target) {
+            return right - left + 1;
+        }
+        return 0;
+    }
+
+    public static int binarySearch(int[] array, int target, boolean isLow) {
+        int left = 0;
+        int right = array.length - 1;
+        int result = array.length;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (array[mid] > target || (isLow && array[mid] >= target)) {
+                right = mid - 1;
+                result = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+
+
+    static HashMap<Integer, Integer> map = new HashMap<>();
     static int result = -1;
+
     public static int findRepeatNumber(int[] nums) {
-        for (int i :nums){
-            if(map.containsKey(i)){
-                map.put(i,map.get(i)+1);
-            }else{
-                map.put(i,1);
+        for (int i : nums) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
             }
         }
 
         map.forEach((key, value) -> {
-            System.out.println("key:"+key+"----value:"+value);
-            if(value>1){
+            System.out.println("key:" + key + "----value:" + value);
+            if (value > 1) {
                 result = key;
             }
         });
         return result;
+    }
+
+
+    public static int findRepeatNumber1(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int num = array[i];
+            if (num == i) {
+                //说明num在正确的位置上
+                continue;
+            }
+            if (array[num] == num) {
+                //说明当前位置已经有这个数字。
+                return num;
+            }
+            //下标循环移位，将num放到自己所在的位置上。
+            int position = num;
+            while (array[position] != position) {
+                int temp = array[position];
+                array[position] = position;
+                position = temp;
+            }
+        }
+        return 0;
     }
 
 
---
+++ lib/src/main/java/j/com/lib/offer/offer_55_search.java
@@ -0,0 +1,14 @@
+package j.com.lib.offer;
+
+/**
+ * @author     : zhupp
+ * @date       : 2021/9/7
+ * description : 在排序数组中查找数字
+ * 统计一个数字在排序数组中出现的次数。
+ */
+public class offer_55_search {
+
+    public  int search(int[] array, int target) {
+        return 0;
+    }
+}
