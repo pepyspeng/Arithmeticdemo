01f43521d13d56bce3e5499eaa30a13fc1ab82fa
zhu.pengpeng <zhu.pengpeng@immomo.com>
2021-10-25 22:31:57 +0800

    二叉搜索树与双向链表

--- lib/src/main/java/j/com/lib/offer/offer_36_Convert.java
+++ lib/src/main/java/j/com/lib/offer/offer_36_Convert.java
@@ -1,17 +1,66 @@
 package j.com.lib.offer;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import j.com.lib.bean.Node;
-import j.com.lib.bean.TreeNode;
 
 /**
  * 搜索二叉树和双相链表
  */
 public class offer_36_Convert {
-
+    /**
+     * 相当于尾节点
+     */
+    Node pre;
+    /**
+     * 相当于头结点
+     */
     Node head;
+
+    public Node treeToDoublyList(Node root) {
+        if (root == null) {
+            return null;
+        }
+        midDfs(root);
+        //要求头尾相接
+        head.left = pre;
+        pre.right = head;
+        return head;
+    }
+
+    /**
+     * 中序遍历，然后让中间节点，指向前后
+     *
+     * @param root
+     */
+    private void midDfs(Node root) {
+        if (root == null) {
+            return;
+        }
+        midDfs(root.left);
+        if (pre == null) {
+            //说明这里是头结点
+            head = root;
+        } else {
+            //上一个节点的右节点指向当前节点
+            pre.right = root;
+        }
+        //当前节点的左节点指向上一节点
+        root.left = pre;
+        pre = root;
+        midDfs(root.right);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+   /* Node head;
     Node pre;
     public Node treeToDoublyList(Node root) {
         if(root == null){
@@ -24,10 +73,10 @@ public class offer_36_Convert {
         return  head;
     }
 
-    /**
+    *//**
      * 中序遍历
      * @param cur
-     */
+     *//*
     private void dfs(Node cur) {
         if(cur == null){
             return;
@@ -78,5 +127,5 @@ public class offer_36_Convert {
         getMid(root.left);
         mid.add(root);
         getMid(root.right);
-    }
+    }*/
 }
---
+++ lib/src/main/java/j/com/lib/offer/offer_54_kthLargest.java
@@ -0,0 +1,50 @@
+package j.com.lib.offer;
+
+
+import j.com.lib.bean.TreeNode;
+
+/**
+ * 二叉搜索树的第k大节点
+ * 给定一棵二叉搜索树，请找出其中第k大的节点。
+ */
+public class offer_54_kthLargest {
+
+
+    int result = 0;
+    int position = 0;
+
+    /**
+     * 二叉搜索树的中序遍历是递增的，那么可以倒着遍历，既能得到递减的，
+     *
+     * @param root
+     * @param k
+     * @return
+     */
+    public int kthLargest(TreeNode root, int k) {
+        position = k;
+        dfs(root);
+        return result;
+    }
+
+    /**
+     * 中序是  左根右
+     * 倒着是  右根左
+     * @param root
+     */
+    private void dfs(TreeNode root) {
+        if(root == null){
+            return;
+        }
+        dfs(root.right);
+        if(position == 0){
+            // 若 k = 0k=0 ，代表已找到目标节点，无需继续遍历，因此直接返回；
+            return;
+        }
+        //执行 k = k - 1k=k−1 （即从 kk 减至 00 ）
+        if(--position == 0){
+            // 若 k = 0k=0 ，代表当前节点为第 kk 大的节点，因此记录 res = root.valres=root.val ；
+            result = root.val;
+        }
+        dfs(root.left);
+    }
+}
